5c45e1b04bcda2db55dec918a3f97fe423f1f565
Ahmed Hossam <ahmmedwahba@yahoo.com>
2025-11-16 01:43:46 +0200

    Delete Student.java

--- Student.java
+++
@@ -1,7 +0,0 @@
-package Backend;
-
-public class Student extends User {
-    public Student(String userId, String username, String email, String passwordHash) {
-        super(userId, "student", username, email, passwordHash);
-    }
-}
