377e68647ce5f735f16f9a35b8863a2a18ff112b
tango12x <galaltaman123@gmail.com>
2025-11-23 08:06:24 +0200

    more stable version !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/frontend/Login.java
+++ src/frontend/Login.java
@@ -3,6 +3,7 @@ package frontend;
 import backend.databaseManager.*;
 import backend.models.parents.User;
 import backend.services.AuthManager;
+import frontend.Instructor.*;
 
 
 import javax.swing.JFrame;
